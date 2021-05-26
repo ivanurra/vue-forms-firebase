28916e0eb85d01ec528275bc04e5e08502d0c336
ivanurra <ivan.urra.wd@gmail.com>
2021-05-26 09:10:32 +0200

    beauty

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,13 @@
 <template>
   <div class="navbar navbar-dark bg-dark">
-      <router-link to="/" class="navbar-brand">
-          Formularios
-      </router-link>
-      <div class="d-flex">
-          <router-link class="btn btn-dark" to="/">
-            Tareas
-          </router-link>
-      </div>
+    <router-link to="/" class="navbar-brand"> Formularios </router-link>
+    <div class="d-flex">
+      <router-link class="btn btn-dark" to="/"> Tareas </router-link>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
--- src/components/TasksList.vue
+++ src/components/TasksList.vue
@@ -1,5 +1,5 @@
 <template>
-{{tasks}}
+  {{ tasks }}
   <table class="table">
     <thead>
       <tr>
@@ -13,32 +13,30 @@
     </thead>
     <tbody>
       <tr v-for="item in tasks" :key="item.id">
-        <th scope="row">{{item.id}}</th>
-        <td>{{item.name}}</td>
+        <th scope="row">{{ item.id }}</th>
+        <td>{{ item.name }}</td>
         <td>
-            <span v-for="(cat, index) in item.categories" :key="index">
-                {{
-                    (item.categories.length === index + 1) ? cat : cat + ', '
-                }}
-            </span>
+          <span v-for="(cat, index) in item.categories" :key="index">
+            {{ item.categories.length === index + 1 ? cat : cat + ", " }}
+          </span>
         </td>
-        <td>{{item.status}}</td>
-        <td>{{item.number}}</td>
+        <td>{{ item.status }}</td>
+        <td>{{ item.number }}</td>
         <td>
-            <button class="btn btn-danger btn-sm" @click="deleteTasks(item.id)">
-                Delete
-            </button>
-            <router-link
+          <button class="btn btn-danger btn-sm" @click="deleteTasks(item.id)">
+            Delete
+          </button>
+          <router-link
             class="btn btn-warning ml-2 btn-sm"
             :to="{
-                name: 'Edit',
-                params: {
-                    id: item.id
-                }
+              name: 'Edit',
+              params: {
+                id: item.id,
+              },
             }"
-            >
-                Edit
-            </router-link>
+          >
+            Edit
+          </router-link>
         </td>
       </tr>
     </tbody>
@@ -46,14 +44,14 @@
 </template>
 
 <script>
-import {mapState, mapActions} from 'vuex'
+import { mapState, mapActions } from "vuex";
 
 export default {
-    computed: {
-        ...mapState(['tasks']),
-    },
-    methods: {
-        ...mapActions(['deleteTasks'])
-    }
+  computed: {
+    ...mapState(["tasks"]),
+  },
+  methods: {
+    ...mapActions(["deleteTasks"]),
+  },
 };
 </script>
