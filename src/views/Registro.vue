012d7af19528dabbe7008788b7a29a85fa6c72ca
ivanurra <ivan.urra.wd@gmail.com>
2021-06-17 09:21:56 +0200

    registrar section

---
+++ src/views/Registro.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="my-5">Registro de usuarios</h1>
+  <form action="">
+    <input type="email" placeholder="email" class="form-control my-2"/>
+    <input type="password" placeholder="password" class="form-control my-2"/>
+    <input type="password" placeholder="password" class="form-control my-2"/>
+    <button type="submit" class="btn btn-primary">Registrar</button>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        
+        }
+    },
+};
+</script>
