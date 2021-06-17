c59e8cfae2bc5dd868a5c3533f318c88bc57ddae
ivanurra <ivan.urra.wd@gmail.com>
2021-06-17 10:30:14 +0200

    validation front

--- src/views/Registro.vue
+++ src/views/Registro.vue
@@ -1,10 +1,10 @@
 <template>
   <h1 class="my-5">Registro de usuarios</h1>
   <form action="">
-    <input type="email" placeholder="email" class="form-control my-2"/>
-    <input type="password" placeholder="password" class="form-control my-2"/>
-    <input type="password" placeholder="password" class="form-control my-2"/>
-    <button type="submit" class="btn btn-primary">Registrar</button>
+    <input type="email" placeholder="email" class="form-control my-2" v-model.trim="email" />
+    <input type="password" placeholder="password" class="form-control my-2" v-model.trim="pass1" />
+    <input type="password" placeholder="password" class="form-control my-2" v-model.trim="pass2" />
+    <button type="submit" class="btn btn-primary" :disabled="bloquear">Registrar</button>
   </form>
 </template>
 
@@ -12,8 +12,21 @@
 export default {
     data() {
         return {
-        
+            email: '',
+            pass1: '',
+            pass2: '',
         }
     },
+    computed: {
+        bloquear(){
+            if(!this.email.includes('@')) {
+                return true
+            }
+            if(this.pass1.length > 5 && this.pass1 === this.pass2) {
+                return false
+            }
+            return true
+        }
+    }
 };
 </script>
