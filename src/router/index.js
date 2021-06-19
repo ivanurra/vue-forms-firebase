1bd4bcc1947c5444a8aa38cd84dbe3e8b3bbcb68
Ivan Urra <ivan.urra.wd@gmail.com>
2021-06-19 13:04:16 +0200

    protected routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import store from '../store'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('../views/About.vue')
+    component: Home,
+    meta: {rutaProtegida: true}
   },
   {
     path: '/editar/:id',
     name: 'Editar',
-    component: () => import('../views/Editar.vue')
+    component: () => import('../views/Editar.vue'),
+    meta: {rutaProtegida: true}
   },
   {
     path: '/registro',
@@ -32,6 +30,19 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  console.log(to.meta.rutaProtegida)
+  if(to.meta.rutaProtegida) {
+    if (store.getters.usuarioAutenticado) {
+      next()
+    } else {
+      next ('/ingreso')
+    }
+  } else {
+    next()
+  }
 })
 
 export default router
